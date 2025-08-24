332cd50d1de4368530f48ff73ce69930730e081c
Sarthak singh <sarthaksinghsps.23@gmail.com>
2025-08-24 19:48:41 +0530

    vercel added

--- app/utils/newRequest.js
+++ app/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://devlinks-backend-1.onrender.com/api/auth",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
